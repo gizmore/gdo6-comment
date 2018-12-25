c058ceda01030f1c0e9992f20e49e3f806d32ce1
gizmore <gizmore@wechall.net>
2018-12-26 00:46:23 +0100

    6.08-r97_fixes

--- lang/comments_en.php
+++ lang/comments_en.php
@@ -5,8 +5,10 @@ return array(
 'commented_at' => 'comment from %s',
 ###########
 'msg_comment_added' => 'Your comment has been added.',
-####
+###########
 'ft_comments_edit' => '[%s] Edit Comment',
-####
+###########
 'list_comments' => '[%s] %s Comments',
+###########
+'mail_new_comment_title' => '[%s] New Comment',
 );
