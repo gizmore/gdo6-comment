ba2d48f31d764241f0a7b6b043d70f1a2a520b13
gizmore <gizmore@wechall.net>
2019-08-31 16:25:19 +0200

    6.10-r42_german_lang

---
+++ lang/comments_de.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+'gdo_comment' => 'Comment',
+'link_comments' => '%s comments…',
+'btn_write_comment' => 'Write&nbsp;Comment',
+'commented_at' => 'comment from %s',
+'comment_top' => 'Top comment?',
+###########
+'cfg_comment_email' => 'Send mail on new comments.',
+'cfg_comment_approval' => 'Enable approval engine.',
+###########
+'msg_comment_added' => 'Your comment has been added.',
+###########
+'ft_comment_edit' => 'Edit Comment',
+'msg_comment_edited' => 'The comment has been edited.',
+###########
+'list_comments' => '%s Comments',
+###########
+'mail_new_comment_title' => '[%s] New Comment',
+);
--- lang/comments_en.php
+++ lang/comments_en.php
@@ -1,20 +1,20 @@
 <?php
 return array(
-'gdo_comment' => 'Comment',
-'link_comments' => '%s comments…',
-'btn_write_comment' => 'Write&nbsp;Comment',
-'commented_at' => 'comment from %s',
-'comment_top' => 'Top comment?',
+'gdo_comment' => 'Kommentar',
+'link_comments' => '%s Kommetare…',
+'btn_write_comment' => 'Kommentar verfassen',
+'commented_at' => 'Kommentar von %s',
+'comment_top' => 'Top Kommentar?',
 ###########
-'cfg_comment_email' => 'Send mail on new comments.',
-'cfg_comment_approval' => 'Enable approval engine.',
+'cfg_comment_email' => 'Email senden bei einem neuen Kommentar?',
+'cfg_comment_approval' => 'Kommentar müssen freigeschaltet werden?',
 ###########
-'msg_comment_added' => 'Your comment has been added.',
+'msg_comment_added' => 'Ihr Kommentar wurde hinzugefügt.',
 ###########
-'ft_comment_edit' => 'Edit Comment',
-'msg_comment_edited' => 'The comment has been edited.',
+'ft_comment_edit' => 'Kommentar bearbeiten',
+'msg_comment_edited' => 'Der Kommentar wurde bearbeitet.',
 ###########
-'list_comments' => '%s Comments',
+'list_comments' => '%s Kommentare',
 ###########
-'mail_new_comment_title' => '[%s] New Comment',
+'mail_new_comment_title' => '[%s] Neuer Kommentar',
 );
